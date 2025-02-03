8d03b1f76861903bb9ceade2f0dacaa3ff013d69
Muhammad Riza <riza.muhammad.x@gmail.com>
2025-02-03 15:44:26 +0700

    fix: fix max upload size, update cetak page

--- cetak.php
+++ cetak.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
 
-	include 'koneksi.php';
+include 'koneksi.php';
 
-	$peserta = mysqli_query($conn, "SELECT * FROM tb_pendaftaran WHERE id_pendaftaran = '".$_GET['id']."'");
-	$p = mysqli_fetch_object($peserta);
+$peserta = mysqli_query($conn, "SELECT * FROM tb_pendaftaran WHERE id_pendaftaran = '" . $_GET['id'] . "'");
+$p = mysqli_fetch_object($peserta);
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,44 +19,38 @@
 		window.print();
 	</script>
 </head>
+
 <body>
 
 	<h2>Bukti Pendaftaran</h2>
 	<table class="table-cetak" border="0">
+		<tr>
+			<td><img src="<?php echo $p->foto ?>" style="height:100px;width:100px;" /></td>
+		</tr>
 		<tr>
 			<td>Kode Pendaftaran</td>
 			<td>:</td>
 			<td><?php echo $p->id_pendaftaran ?></td>
 		</tr>
 		<tr>
-			<td>Tahun Ajaran</td>
-			<td>:</td>
-			<td><?php echo $p->th_ajaran ?></td>
-		</tr>
-		<tr>
-			<td>Jurusan</td>
-			<td>:</td>
-			<td><?php echo $p->jurusan ?></td>
-		</tr>
-		<tr>
 			<td>Nama Lengkap</td>
 			<td>:</td>
-			<td><?php echo $p->nm_peserta ?></td>
+			<td><?php echo $p->nama ?></td>
 		</tr>
 		<tr>
 			<td>Tempat Lahir</td>
 			<td>:</td>
-			<td><?php echo $p->tmp_lahir ?></td>
+			<td><?php echo $p->tempat_lahir ?></td>
 		</tr>
 		<tr>
 			<td>Tanggal Lahir</td>
 			<td>:</td>
-			<td><?php echo $p->tgl_lahir ?></td>
+			<td><?php echo $p->tanggal_lahir ?></td>
 		</tr>
 		<tr>
 			<td>Jenis Kelamin</td>
 			<td>:</td>
-			<td><?php echo $p->jk ?></td>
+			<td><?php echo $p->jenis_kelamin ?></td>
 		</tr>
 		<tr>
 			<td>Agama</td>
@@ -65,9 +60,9 @@
 		<tr>
 			<td>Alamat</td>
 			<td>:</td>
-			<td><?php echo $p->almt_peserta ?></td>
+			<td><?php echo $p->alamat ?></td>
 		</tr>
 	</table>
-
 </body>
+
 </html>
