71a8613b2df7f6dfb21c58757ee88569663edae5
Muhammad Riza <riza.muhammad.x@gmail.com>
2025-02-24 08:40:19 +0700

    fix: fix redirect after delete registrant

--- hapus-peserta.php
+++ hapus-peserta.php
@@ -1,12 +1,10 @@
-<?php 
+<?php
 
-	include 'koneksi.php';
-	if($_SESSION['stat_login'] != true){
-		echo '<script>window.location="login.php"</script>';
-	}
-	if(isset($_GET['id'])){
-		$delete = mysqli_query($conn, "DELETE FROM tb_pendaftaran WHERE id_pendaftaran = '".$_GET['id']."' ");
-		echo '<script>window.location="data-peserta.php"</script>';
-	}
-
-?>
+include 'koneksi.php';
+if ($_SESSION['stat_login'] != true) {
+	header("Location: login.php");
+}
+if (isset($_GET['id'])) {
+	$delete = mysqli_query($conn, "DELETE FROM tb_pendaftaran WHERE id_pendaftaran = '" . $_GET['id'] . "' ");
+	header("Location: data-peserta.php");
+}
